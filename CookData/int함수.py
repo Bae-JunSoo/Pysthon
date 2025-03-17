a2086f465a30d4b9863f317f92d91981dd64fc5f
Baejunsoo <junsoo9023@gmail.com>
2025-03-17 11:35:33 +0900

    Add files via upload

---
+++ CookData/int함수.py
@@ -0,0 +1,23 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> var1 = input()
+100
+>>> var2 = input()
+200
+>>> print(var1 + var2)
+100200
+>>> 
+>>> var1 = int(input())
+100
+>>> var2 = int(input())
+200
+>>> print(var1 + var2)
+300
+>>> 
+>>> type(var1)
+<class 'int'>
+>>> 
+>>> name = input("이름을 입력하세요==>")
+이름을 입력하세요==>배준수
+>>> print(name)
+배준수
---
+++ CookData/관계 함수.py
@@ -0,0 +1,13 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> a, b = 100, 200
+>>> 
+>>> print(a==b)
+False
+>>> print(a<=b)
+True
+>>> 
+>>> c = a + b
+>>> print(c <= a + b)
+True

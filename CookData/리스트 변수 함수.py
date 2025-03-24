4781c2799eee1a992f0b2c9d84e0965812577752
빅데이터과 배준수 <junsoo9023@gmail.com>
2025-03-24 11:17:45 +0900

    Add files via upload

---
+++ CookData/리스트 변수 함수.py
@@ -0,0 +1,7 @@
+aa = []
+
+aa.append(0)
+aa.append(1)
+aa.append(2)
+aa.append(3)
+print(aa)
---
+++ CookData/리스트 함수 응용.py
@@ -0,0 +1,7 @@
+aa = []
+
+for i in range(0, 100):
+    aa.append(i)
+    print(aa[i])
+
+## aa.append() = 변수 추가 ##

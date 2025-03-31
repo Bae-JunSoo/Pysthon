d875b43bc7416462f1d7b59c22545cfc11c9a9b9
빅데이터과 배준수 <junsoo9023@gmail.com>
2025-03-31 12:31:59 +0900

    Add files via upload

---
+++ 선형 리스트/선형 리스트 생성 2.py
@@ -0,0 +1,32 @@
+katok = ["다현","정연","쯔위","사나","지효"]
+print(katok)
+
+katok.append(None)
+print(katok)
+
+katok[5] = "모모"
+print(katok)
+
+katok.append(None)
+print(katok)
+
+katok[6] = katok[5]
+print(katok)
+
+katok[5] = None
+print(katok)
+
+katok[5] = katok[4]
+print(katok)
+
+katok[4] = None
+print(katok)
+
+katok[4] = katok[3]
+print(katok)
+
+katok[3] = None
+print(katok)
+
+katok[3] = "미나"
+print(katok)

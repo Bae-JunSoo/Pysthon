b1749d2d5992d39f9d737d04b9b73c7254fd9031
빅데이터과 배준수 <junsoo9023@gmail.com>
2025-03-31 12:32:19 +0900

    Add files via upload

---
+++ 선형 리스트/배열을 이용한 선형 리스트.py
@@ -0,0 +1,32 @@
+katok = []
+print(katok)
+
+print(len(katok))
+
+katok.append(None)
+print(len(katok))
+
+print(katok)
+
+katok[0] = "다현"
+print(katok)
+
+배열길이 = len(katok)
+print(배열길이)
+
+katok[배열길이-1] = "다현"
+print(katok)
+
+katok.append(None)
+배열길이 = len(katok)
+print(배열길이)
+
+katok[배열길이-1] = "정연"
+print(katok)
+
+katok.append(None)
+배열길이 = len(katok)
+print(배열길이)
+
+katok[배열길이-1] = "준수"
+print(katok)
